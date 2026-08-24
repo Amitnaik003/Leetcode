65b59359422603e4eef5d6015cf8adc51e2537d0
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2026-08-24 19:40:27 +0530

    Time: 3 ms (100%), Space: 83.1 MB (98.72%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -2,12 +2,12 @@ class Solution {
     public int stoneGameVIII(int[] A) {
         int n = A.length;
         for (int i = 1; i < n; i++)
-            A[i] += A[i - 1];
+            A[i] = A[i] +  A[i - 1];
 
-        int ans = A[n - 1];
+        int dp = A[n - 1];
         for (int i = n - 2; i > 0; i--)
-            ans = Math.max(ans, A[i] - ans);
+            dp = Math.max(dp, A[i] - dp);
 
-        return ans;
+        return dp;
     }
 }
